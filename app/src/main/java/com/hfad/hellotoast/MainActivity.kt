2904bf6f06d0dbce1e98bd027646c6d776db2d08
Ekaterina Pavlova <ekaterinapavlova_96@mail.ru>
2021-11-03 13:17:37 +0300

    Created funs onClick

--- app/src/main/java/com/hfad/hellotoast/MainActivity.kt
+++ app/src/main/java/com/hfad/hellotoast/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.hfad.hellotoast
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showToast(view: android.view.View) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+    }
+
+
+    fun countUp(view: android.view.View) {
+        mCount ++
+        val mShowCount = findViewById<TextView>(R.id.show_count)
+        mShowCount.text = mCount.toString()
     }
 }
