09152c039a35b8aa8e542fa5997aa52316c8c46f
Ekaterina Pavlova <ekaterinapavlova_96@mail.ru>
2021-11-03 13:56:29 +0300

    Added funs for save instance state

--- app/src/main/java/com/hfad/hellotoast/MainActivity.kt
+++ app/src/main/java/com/hfad/hellotoast/MainActivity.kt
@@ -9,20 +9,37 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private var mCount = 0
+    private lateinit var mShowCount: TextView
+
+    companion object {
+        private const val KEY_COUNT = "KEY_COUNT"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mShowCount = findViewById(R.id.show_count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        mCount = savedInstanceState.getInt(KEY_COUNT)
+        mShowCount.text = mCount.toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_COUNT, mCount)
     }
 
     fun showToast(view: android.view.View) {
         Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
     }
 
-
     fun countUp(view: android.view.View) {
         mCount ++
-        val mShowCount = findViewById<TextView>(R.id.show_count)
         mShowCount.text = mCount.toString()
     }
+
 }
